ea70cd0cc0e9a5f88588513f2ffce24370499444
Smarpit <smarpitsingh560@gmail.com>
2021-05-23 08:12:19 +0530

    layers setup

---
+++ lib/DataLayer/location.dart
@@ -0,0 +1,10 @@
+class Location {
+  final int id;
+  final String type;
+  final String title;
+
+  Location.fromJson(Map json)
+      : id = json['entity_id'],
+        type = json['entity_type'],
+        title = json['title'];
+}
